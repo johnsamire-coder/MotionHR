021d69131d5bc7f14874ee2eae300eaa7cf5f038
johnsamire-coder <johnsamire@gmail.com>
2026-08-24 15:17:15 +0300

    fix(import): add JWTAuthentication import in employees/views.py

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,4 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
